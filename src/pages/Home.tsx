5782248ab3a614a03b9349bd6a8060a46a0ff6e5
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2024-10-01 11:12:56 -0500

    add new section home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,20 +6,34 @@ import '../styles/home.css'
 
 const Home = () => {
   const articleService = new ArticleService()
-  const [articles, setArticles] = useState<ArticleInterface[]>([])
+  const [articlesContainer1, setArticlesContainer1] = useState<ArticleInterface[]>([])
+  const [articlesContainer2, setArticlesContainer2] = useState<ArticleInterface[]>([])
   const [error, setError] = useState<boolean>(false)
 
   useEffect(() => {
     const getArticles = async () => {
-      const articles = await articleService.getArticles({limit: 7, offset: 0})
-      if (articles.error) setError(true)
-      setArticles(articles)
+      const results = await Promise.allSettled([
+        articleService.getArticles({ limit: 7, offset: 0 }),
+        articleService.getArticles({ limit: 10, offset: 7 }),
+      ]);
+    
+      const initialArticles = results[0];
+      const secondArticles = results[1];
+      console.log(initialArticles, secondArticles);
+    
+      if (initialArticles.status === 'rejected' || secondArticles.status === 'rejected') {
+        setError(true); 
+        return; 
+      }
+
+      setArticlesContainer1(initialArticles.value);
+      setArticlesContainer2(secondArticles.value);
     }
 
     getArticles()
   }, [])
 
-  if (error || !articles.length) {
+  if (error || !articlesContainer1.length || !articlesContainer2.length) {
     return (
       <div>
         <h1>No Articles</h1>
@@ -31,12 +45,22 @@ const Home = () => {
     <div className="home">
       <div className='container'>
         <div className="main">
-          {articles.map((article, index) => (
+          {articlesContainer1.map((article, index) => (
             <Card article={article} customClass={index === 0 ? 'card_article_horizontal' : ''} key={article.id} />
           ))}
         </div>
         <div className="aside">
           publicidad
+        </div>
+      </div>
+      <div className='container'>
+        <div className="main2">
+          {articlesContainer2.map((article) => (
+            <Card article={article} customClass={'card_article_horizontal_left'} key={article.id} />
+          ))}
+        </div>
+        <div className="aside">
+          publicidad 3
         </div>
       </div>
     </div>
